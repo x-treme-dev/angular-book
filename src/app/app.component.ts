9030e60ec4eb629019c9fcae7e365d0f65290cf2
engineer <newgalaxy401@gmail.com>
2024-12-04 17:53:00 +0300

    add components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BookComponent } from './book/book.component';
+import { ReviewComponent } from './review/review.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BookComponent, ReviewComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/review/review.component.ts
+++ src/app/review/review.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './review.component.css'
 })
 export class ReviewComponent {
-
+   
 }
